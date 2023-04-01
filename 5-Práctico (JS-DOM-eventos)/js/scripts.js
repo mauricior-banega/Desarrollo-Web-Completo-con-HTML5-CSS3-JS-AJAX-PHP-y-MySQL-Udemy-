c29892999ba76d0a79f49b757f96a95a6dd4788d
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-03-31 23:49:39 -0300

    10-04
    Tema: Eventos en JavaScript. Cada tema esta explicado dentro del archivo scripts.js.

--- 5-Práctico (JS-DOM-eventos)/js/scripts.js
+++ 5-Práctico (JS-DOM-eventos)/js/scripts.js
@@ -29,13 +29,13 @@ JavaScript en el Navegador y DOM Scripting
 //Estas propiedades de los elementos podremos verlo si desplegamos en el triangulo del elemento h2 desde el Inspeccionador en Firefox Developer.
 //Basicamente desde JS usando estos selectores podremos cambiar/editar todos los elementos del DOM.
 
-    const heading = document.querySelector('.header__texto h2');
+const heading = document.querySelector('.header__texto h2');
 
-    heading.textContent = 'Nuevo Heading';
+heading.textContent = 'Nuevo Heading';
 
 //Otra modificacion comun es agregar una clase
 
-    heading.classList.add('nueva-clase');
+heading.classList.add('nueva-clase');
 
 
 
@@ -48,29 +48,29 @@ querySelectorAll
 
 */
 
-    const enlaces = document.querySelectorAll('.navegacion a');
+const enlaces = document.querySelectorAll('.navegacion a');
 
-    //console.log(enlaces);
+//console.log(enlaces);
 
 //Nos mostrará "NodeList(6)" que son todos los elementos que tiene. Entonces para acceder auno de ellos definimos al igual que como hacemos con los arreglos, especificando entre [] la posicion.
 
-    //console.log(enlaces[0]);
+//console.log(enlaces[0]);
 
 //Aplicando las mismas modificaciones en propiedades:
 
-    enlaces[0].textContent = 'Nuevo texto para enlace';
+enlaces[0].textContent = 'Nuevo texto para enlace';
 
 //Cambiaremos en enlace[0] justamente el enlace que antes direccionaba "nosotros.html".
 
-    enlaces[0].href = 'http://google.com';
+enlaces[0].href = 'http://google.com';
 
 //Agregaremos una clase
 
-    enlaces[0].classList.add('nueva-clase'); //No ponemos punto para indicar que agregaremos una clase ya que se lo indicamos porque pasamos que la accion y en ella trata de una clase. 
+enlaces[0].classList.add('nueva-clase'); //No ponemos punto para indicar que agregaremos una clase ya que se lo indicamos porque pasamos que la accion y en ella trata de una clase. 
 
 //Eliminamos una clase
 
-    enlaces[0].classList.remove('navegacion__enlace');
+enlaces[0].classList.remove('navegacion__enlace');
 
 
 
@@ -78,15 +78,122 @@ querySelectorAll
 
 /* -------------------------------------------------------------------
 
-    getElementById
+getElementById
 -Este selector no se utiliza, se usan mas los primeros 2. Pero explicaremos este.
 
- */
+*/
 
-    const heading2 = document.getElementById('heading'); 
-    //Como ya sabe que estas buscando un ID no hace falta especificar el #.
+const heading2 = document.getElementById('heading'); 
+//Como ya sabe que estas buscando un ID no hace falta especificar el #.
 
-    console.log(heading2);
+console.log(heading2);
 
-    //Veremos como selecciona el h2 porque es el unico elemento que tiene por ID "heading". No ejemplificaremos modificaciones pero claro que pueden aplicarse con este selector tambien.
+//Veremos como selecciona el h2 porque es el unico elemento que tiene por ID "heading". No ejemplificaremos modificaciones pero claro que pueden aplicarse con este selector tambien.
+
+/* -------------------------------------------------------------------
+
+Crear HTML con createElement
+
+Generar un nuevo enlace: 
+
+- Se pasa .createElement y dentro de los () la etiqueta en Mayusculas.
+
+*/
+
+const nuevoEnlace = document.createElement('A');
+
+// console.log(nuevoEnlace);
+
+
+//Veremos que agrega una etiqueta vacia "a", pero un enlace tiene mas elementos. Agregaremos cada uno de ellos (href/texto/clase). Por ultimo lo agregamos al documento.
+//Todo esto nos sirve para darle dinamismo al codigo ya que es mas facil acceder mediante JS para modificar/agregar o eliminar que modificar directamente un HTML o documento quizas formulario, traido desde una DBO.
+
+//Agregamos href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+//Agregamos texto
+nuevoEnlace.textContent = 'Un nuevo enlace';
+
+//Agregamos la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+console.log(nuevoEnlace);
+
+
+
+/* -------------------------------------------------------------------
+
+Eventos
+- Veremos varios eventos asociados al window/documento entre otros del cual mostraremos en consola. Podremos ver como mandamos varios mensajes por consola pero el 2º/3º y al ultimo 5º pero este se muestra antes que los otros mencionados y esto es porque espera la carga de los eventos lanzados, distintos en sintaxis cada uno pero que tienen la misma funcionalidad. Este evento se ejecuta cuando se carga todos los archivos.
+
+*/
+
+console.log(1); //Mensaje 1º
+
+
+window.addEventListener('load', function() //(1)
+{
+    console.log(2); //Mensaje 2º
+
+    //"load", espera a que el JS y los archivos que dependen del HTML (img/videos, etc) esten listos.
+});
+
+
+//Otra forma de expresar la carga con diferente sintaxis
+
+window.onload = function()
+{
+    console.log(3); //Mensaje 3º
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log(4); //Mensaje 4º
+
+    //Solo espera por la carga del HTML, pero no por CSS o imágenes. Por eso es mas rápido, y en este caso que usaremos scripting y trabajaremos sobre HTML y no otros archivos con JS utilizaremos en caso de necesitar, este evento.
+});
+
+
+console.log(5); //Mensaje 5º
+
+/*(1)-En el Mensaje 2º , si quitamos la funcion tipo CallBack que esta y la expresamos por separado, podremos solo poner el nombre de la F() unicamente sin los paréntesis. Ejemplifico:
+
+
+window.addEventListener('load', imprimir)
+
+    function imprimir()
+        {
+            console.log(2);
+        } 
+
+-Aclaramos esto porque sirve hacer esto si la funcion en tiene muchas lineas de codigo es mejor separarlo y no declararlo en la misma sentencia del evento.
+
+*/
+
+
+//Hay otro evento "onscroll" que sirve para contabilizar cuantas veces se scrollea en la página, y mostraremos las veces por consola
+
+window.onscroll = function()
+{
+console.log('scrolling...');
+}
+
+
+
+
+/* -------------------------------------------------------------------
+
+XXXXX
+-
+
+*/
+
+
+
+//Veremos 
 
